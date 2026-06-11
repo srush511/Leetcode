b00123db4873fff80b0ca5194f9ef5dd686db989
Srushti Chikte <65661863+srush511@users.noreply.github.com>
2026-06-11 16:57:40 +0530

    Time: 1465 ms (78.11%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -1,21 +1,13 @@
 /* Write your T-SQL query statement below */
 
 
-with approved_amount as(
-    select FORMAT(trans_date, 'yyyy-MM') AS month,country,count(iif(state ='approved',1,null)) as approved_count, sum(IIF(state = 'approved', amount, 0)) as approved_total_amount from Transactions     
+
+
+    select FORMAT(trans_date,'yyyy-MM') AS month, country,
+    count(id) as trans_count,
+    count(iif(state ='approved',1,null)) as approved_count,    
+    sum(amount) as trans_total_amount,
+    sum(IIF(state = 'approved', amount, 0)) as approved_total_amount
+     from Transactions
     group by FORMAT(trans_date,'yyyy-MM'),country
-),
-
-transaction_count as(
-    select FORMAT(trans_date,'yyyy-MM') AS month, country,count(id) as trans_count,sum(amount) as trans_total_amount from Transactions
-    group by FORMAT(trans_date,'yyyy-MM'),country
-)
-
-
-select aa.month,aa.country,tc.trans_count,aa.approved_count,
-tc.trans_total_amount,aa.approved_total_amount
-from approved_amount aa
-join transaction_count tc
-on aa.month = tc.month and  ISNULL(aa.country, '') = ISNULL(tc.country, '')
-
 
