8469c3416ef4d701e3880ca473e1d7aefdc58332
Srushti Chikte <65661863+srush511@users.noreply.github.com>
2026-06-11 16:22:38 +0530

    Time: 1560 ms (53.50%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -0,0 +1,12 @@
+/* Write your T-SQL query statement below */
+
+
+
+    select FORMAT(trans_date,'yyyy-MM') AS month, country,
+    count(id) as trans_count,
+    count(iif(state ='approved',1,null)) as approved_count,    
+    sum(amount) as trans_total_amount,
+    sum(IIF(state = 'approved', amount, 0)) as approved_total_amount
+     from Transactions
+    group by FORMAT(trans_date,'yyyy-MM'),country
+
