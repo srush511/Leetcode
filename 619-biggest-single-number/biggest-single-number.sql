b08df4a60645d307bcc320ad1975c61164dad50c
Srushti Chikte <65661863+srush511@users.noreply.github.com>
2026-06-13 01:32:08 +0530

    Time: 341 ms (50.36%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,6 +1,8 @@
 /* Write your T-SQL query statement below */
-select max(num) as num from (
-    select num from mynumbers
-group by num
-having count(num)=1
-) as mynumbers
+select max(num) as num
+ from (
+    select num 
+    from mynumbers
+    group by num
+    having count(*)=1
+)  mynumbers
