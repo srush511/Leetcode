07f55d0f0048e9f54dc0f6d0587ec93099429c34
Srushti Chikte <65661863+srush511@users.noreply.github.com>
2026-06-13 01:13:09 +0530

    Time: 650 ms (29.60%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
+++ 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
@@ -1,5 +1,6 @@
 /* Write your T-SQL query statement below */
 select 
     class 
-from courses group by class
-having count(student) >=5
+from courses
+group by class
+    having count(student) >=5
