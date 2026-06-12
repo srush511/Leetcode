c8d2ba95598d50d4395176bdc0bba694f45ae85d
Srushti Chikte <65661863+srush511@users.noreply.github.com>
2026-06-13 01:43:57 +0530

    Time: 1219 ms (88.64%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -0,0 +1,6 @@
+/* Write your T-SQL query statement below */
+select distinct c.customer_id from customer c
+join product p
+on c.product_key = p.product_key
+group by c.customer_id
+having count(distinct c.product_key) = (select count(*) from product)
